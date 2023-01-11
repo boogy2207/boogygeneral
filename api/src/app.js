ee9d67977e10c538e44c8803b55708fc967c990c
juanrnoya <juanrnoya@gmail.com>
2023-01-11 19:47:15 -0300

    mercadoPago

--- api/src/app.js
+++ api/src/app.js
@@ -4,11 +4,14 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
+require("dotenv").config();
+
+const { DB_ACCESS_TOKEN } = process.env;
 // SDK de MercadoPago
 const mercadopago = require("mercadopago");
 // Agrega credenciales mercadoPago
 mercadopago.configure({
-   access_token: "DB_ACCESS_TOKEN",
+   access_token: DB_ACCESS_TOKEN,
 });
 
 require("./db.js");
