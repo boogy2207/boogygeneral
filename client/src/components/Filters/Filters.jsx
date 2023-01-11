87a8afeadf78ffb077e6ed31db9f589ad0d6965e
johaortiz <surf234@outlook.es>
2023-01-11 12:13:03 -0300

    errors correction

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { filter, price, rangePrice } from "../../store/slices/books"
 
-export default function Filtros() {
+export default function Filtros({ paged }) {
 
     const dispatch = useDispatch()
+
     const books = useSelector(state => state.books)
 
     function handleSelect(e) {
@@ -15,10 +15,7 @@ export default function Filtros() {
         else {
             dispatch(filter(e.target.value))
         }
-    }
 
-    function handleRange(e) {
-        dispatch(rangePrice(e.target.value))
     }
 
     function handleRange(e) {
@@ -26,22 +23,22 @@ export default function Filtros() {
     }
 
     return (
-        <div className='mt-10 flex justify-center items-center gap-6'>
+        <div className='m-10 flex'>
 
-            <select onChange={e => handleSelect(e)} className="select select-primary max-w-xs" defaultValue='ASC'>
+            <select onChange={e => handleSelect(e)} className="select select-primary w-full max-w-xs" defaultValue='ASC'>
                 <option disabled>Price</option>
                 <option value="ASC">Mayor a Menor </option>
                 <option value="DESC">Menor a Mayor</option>
             </select>
 
-            <select onChange={e => handleSelect(e)} className="select select-primary max-w-xs" defaultValue='A-Z'>
+            <select onChange={e => handleSelect(e)} className="select select-primary w-full max-w-xs" defaultValue='A-Z'>
                 <option disabled>Alphabetic</option>
                 <option value="A-Z">A-Z</option>
                 <option value="Z-A">Z-A</option>
             </select>
 
 
-            <select onChange={e => handleSelect(e)} className="select select-primary max-w-xs" defaultValue='ALL'>
+            <select onChange={e => handleSelect(e)} className="select select-primary w-full max-w-xs" defaultValue='ALL'>
                 <option disabled>Genre</option>
                 <option value="ALL">ALL</option>
                 {
@@ -51,9 +48,9 @@ export default function Filtros() {
                 }
             </select>
             <div className='m-4 ml-11 flex'>
-            <span className='mr-5'>$0</span>
-            <input type="range" min="0" max="7000" onChange={(e) => handleRange(e)} className="range range-xs" />
-            <span className='ml-5'>$7000</span>
+                <span className='mr-5'>$0</span>
+                <input type="range" min="0" max="7000" onChange={(e) => handleRange(e)} className="range range-xs" />
+                <span className='ml-5'>$7000</span>
             </div>
             <span>Price Range</span>
 
