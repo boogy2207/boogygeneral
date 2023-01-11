f97b4823694afb3d39b53966c1041eabf9a20b9a
francoarenas <franco.argenmu.35@gmail.com>
2023-01-11 02:24:38 -0300

    rangoPrice/carousel

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,12 +1,14 @@
 import Cards from "../Card";
 import NavBar from "../NavBar";
 import Filters from "../Filters/Filters";
+import Carousel from "../Carousel";
 
 export default function Home() {
 
     return (
         <>
             <NavBar />
+            <Carousel/>
             <Filters />
             <Cards />
         </>
