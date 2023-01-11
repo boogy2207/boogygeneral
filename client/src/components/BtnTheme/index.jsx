714528ee50354e8da587102153da48677f8505b3
johaortiz <surf234@outlook.es>
2023-01-11 16:24:12 -0300

    1/2 theme select

--- client/src/components/BtnTheme/index.jsx
+++ client/src/components/BtnTheme/index.jsx
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { changeTheme } from "../../store/slices/theme";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Button from '@mui/material/Button';
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 
 function BtnTheme() {
 
@@ -12,8 +14,17 @@ function BtnTheme() {
         dispatch(changeTheme(themeContext))
     };
 
+    useEffect(() => {
+        themeChange(false)
+        // 👆 false parameter is required for react project
+    }, [])
+
     return (
-        <Button variant="contained" onClick={handleChange}><Brightness4Icon/></Button>
+        <select data-choose-theme>
+            <option value="">Default</option>
+            <option value="lofi">Dark</option>
+            <option value="cupcake">Pink</option>
+        </select>
     );
 };
 export default BtnTheme;
