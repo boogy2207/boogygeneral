da8cb556bb7f57e2938509a4dbdf6d410a516fef
johaortiz <surf234@outlook.es>
2023-01-13 17:25:37 -0300

    deploy v1.0

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/theme";
 import books from "./slices/books";
 import cart from "./slices/cart";
+import user from "./slices/auth";
 
 export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-        books: books,
-        cart: cart
-    },
+  reducer: {
+    theme: themeReducer,
+    books: books,
+    cart: cart,
+    user: user,
+  },
 });
